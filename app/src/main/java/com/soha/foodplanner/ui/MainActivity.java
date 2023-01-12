62664f873def3c5d5ee4d966494b80c2874ac85c
Soha-Ahmed-Hamdy <soha7hmed@gmail.com>
2023-01-12 18:45:16 +0200

    add: firebase

--- app/src/main/java/com/soha/foodplanner/ui/MainActivity.java
+++ app/src/main/java/com/soha/foodplanner/ui/MainActivity.java
@@ -1,13 +1,21 @@
-package com.soha.foodplanner;
+package com.soha.foodplanner.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.soha.foodplanner.R;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
